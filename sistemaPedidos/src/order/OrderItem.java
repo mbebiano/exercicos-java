c019358e6586f3a3ef219e9872f6aa79ee42e2ea
mbebiano <mattheus.bebiano@engenharia.ufjf.br>
2020-09-08 14:20:30 -0300

    Sistema de gerenciamento de pedidos

---
+++ sistemaPedidos/src/order/OrderItem.java
@@ -0,0 +1,33 @@
+package order;
+
+public class OrderItem {
+	private Integer quantity;
+	private Double price;
+	public OrderItem() {
+
+	}
+
+	public OrderItem(Integer quantity, Double price) {
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+	
+	public Double subtotal() {
+		
+		return price*quantity;
+	}
+	
+
+}
---
+++ sistemaPedidos/src/program/Application.java
@@ -0,0 +1,9 @@
+package program;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
