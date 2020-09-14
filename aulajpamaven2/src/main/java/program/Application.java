232fe2be354a09a887efc8573e53b54ddc0f998d
mbebiano <mattheus.bebiano@engenharia.ufjf.br>
2020-09-14 10:39:20 -0300

    Busca por id no banco de dados

--- aulajpamaven2/src/main/java/program/Application.java
+++ aulajpamaven2/src/main/java/program/Application.java
@@ -9,22 +9,19 @@ import entities.Pessoa;
 public class Application {
 
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "João da Pereira", "joao@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Maria de Almeida", "maria@gmail.com");
+		
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
+		Pessoa p = em.find(Pessoa.class, 2);
 		
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		
-		em.getTransaction().commit();
+		System.out.println(p);
 		
 		System.out.println("Finish");
+		
+		em.close();
+		emf.close();
 		
 		
 	}
